49aa42b9692f1ad18d084287c4dafbc7fc37cf89
balavembu <90377726+balavembu@users.noreply.github.com>
2021-12-20 09:04:22 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "set-prj-boolean-policy" {
   source      = "github.com/balavembu/terraform-google-org-policy-boolean"
   policy_for  = "project"
   project_id = "scert-332222"
-  enforce    = true
+  enforce    = false
   constraint  = "compute.disableSerialPortAccess"
   policy_type = "boolean"
 }
